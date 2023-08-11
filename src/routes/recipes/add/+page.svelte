d0a97475c8e350d8fb1a6b733c216b15a7c3d3b0
Danny-Robinson <dannyrobinson94@hotmail.com>
2023-08-11 11:27:09 +0100

    Update +page.svelte

--- src/routes/recipes/add/+page.svelte
+++ src/routes/recipes/add/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+    import { ingredientsStore } from '$lib/stores/ingredientsStore';
+    import { get } from 'svelte/store';
+
     let recipeName = '';
     let cookingTime = '';
     let servings = 1;
@@ -7,35 +10,81 @@
 
     function addInstruction() {
         instructions.push('');
+        instructions = instructions; // Trigger reactivity
     }
 
     function addIngredient() {
         ingredients.push({ name: '', quantity: '', unit: '' });
+        ingredients = ingredients; // Trigger reactivity
     }
 
     async function handleSubmit() {
-        // Here we'll handle the logic to send the data to the backend
-        // Will fill this out soon!
+        const recipeData = {
+        name: recipeName,
+        cookingTime,
+        servings, // Currently hardcoded to 1
+        instructions,
+        ingredients
+    };
+
+    console.log("Submitted Recipe Data:", recipeData);
     } 
-</script>
+
+    function removeIngredient(index) {
+        ingredients.splice(index, 1);
+        ingredients = ingredients; // Trigger reactivity
+    }   
+
+    function removeInstruction(index) {
+        instructions.splice(index, 1);
+        instructions = instructions; // Trigger reactivity
+    }
+
+    let allIngredients = []; // This will store the fetched ingredients list
+
+    // On mount, get ingredients from the store
+    import { onMount } from 'svelte';
+    onMount(() => {
+        allIngredients = get(ingredientsStore);
+    });
+    $: allIngredients = $ingredientsStore;
+ </script>
 
 <form on:submit|preventDefault={handleSubmit}>
     <input bind:value={recipeName} placeholder="Recipe Name" />
     <input type="number" bind:value={cookingTime} placeholder="Cooking Time (mins)" />
     
-    <!-- Loop through instructions and create an input for each one -->
+   <!-- Instructions -->
+   <ul>
     {#each instructions as instruction, i}
-        <input bind:value={instructions[i]} placeholder={`Instruction ${i + 1}`} />
+        {#if i !== instructions.length - 1}
+            <li>
+                {instruction}
+                <button on:click|preventDefault={() => removeInstruction(i)}>Delete</button>
+            </li>
+        {:else}
+            <input bind:value={instructions[i]} placeholder={`Instruction ${i + 1}`} />
+        {/if}
     {/each}
-    <button on:click|preventDefault={addInstruction}>Add Instruction</button>
+</ul>
+<button on:click|preventDefault={addInstruction}>Add Instruction</button>
 
-    <!-- Loop through ingredients and create inputs for each one -->
+
     {#each ingredients as ingredient, i}
-        <input bind:value={ingredients[i].name} placeholder="Ingredient Name" />
-        <input type="number" bind:value={ingredients[i].quantity} placeholder="Quantity" />
-        <input bind:value={ingredients[i].unit} placeholder="Unit (e.g. cup, tsp, g)" />
+    <div class="ingredient-item">
+        <input bind:value={ingredient.name} placeholder="Ingredient Name" list="ingredient-list" />
+        <input type="number" bind:value={ingredient.quantity} placeholder="Quantity" />
+        <input bind:value={ingredient.unit} placeholder="Unit (e.g. cup, tsp, g)" />
+        <button on:click|preventDefault={() => removeIngredient(i)}>Delete</button>
+    </div>
     {/each}
+    <datalist id="ingredient-list">
+        {#each allIngredients as item}
+            <option value={item.name}></option>
+        {/each}
+    </datalist>
     <button on:click|preventDefault={addIngredient}>Add Ingredient</button>
+
 
     <button type="submit">Save Recipe</button>
 </form>
